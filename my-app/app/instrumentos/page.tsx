95d9a1a99d522645400b7b19ea6e470334dd93c4
Yago Emanuel <yagoemanuel@gmail.com>
2025-03-17 16:29:10 -0300

    ..

--- my-app/app/instrumentos/page.tsx
+++ my-app/app/instrumentos/page.tsx
@@ -32,7 +32,12 @@ export default function Page() {
                     <div className="sm:col-span-3">
                         <label htmlFor="valor_unitario" className="block text-sm/6 font-medium text-gray-900">Tipo</label>
                         <div className="mt-2">
-                            <input type="text" value={tipo} onChange={(event) => setTipo(event.target.value)} name="valor_unitario" id="valor_unitario" autoComplete="given-name" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
+                            <select name="tipo" id="tipo" value={tipo} onChange={(event) => setTipo(event.target.value)}  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+                                <option value="">Selecione</option>
+                                <option value="Cordas">Cordas</option>
+                                <option value="Madeira">Madeira</option>
+                                <option value="Metais">Metais</option>
+                            </select>
                         </div>
                     </div>
                 </div>
